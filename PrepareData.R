a1e3aa961cdab39635344251617a70c7eb314d22
silviapineda <silviapinedasanjuan@gmail.com>
2019-04-11 17:15:33 +0200

    Prepared clones and entropy

--- PrepareData.R
+++ PrepareData.R
@@ -14,19 +14,127 @@ print(x)
 ### Author: Silvia Pineda
 ### Date: April, 2019
 ############################################################################################
+library(pgirmess)
+library(stringr)
 
 setwd("~/TCGA-Immune/")
 
-####Read repertoire data
+########## Read the alignment files
+files <- list.files("Data/PAAD/MIXCR_PAAD/alignments/")
+data<-c()
+for(i in files) {
+  cat(i, "\n")
+  t <- read.delim(paste0("Data/PAAD/MIXCR_PAAD/alignments/",i))
+  t$sample<-substr(i, 1, nchar(i)-15)
+  data <- rbind(data, t)
+}
+save(data,file="Data/PAAD/PAAD_MIXCR_results.Rdata")
+
+#####Filter by CDR3
+load("Data/PAAD/PAAD_MIXCR_results.Rdata")
+data_full_cdr3<-data[which(data$nSeqCDR3!=""),] #2,087,012
+data_full_cdr3$CDR3_length<-nchar(as.character(data_full_cdr3$nSeqCDR3)) 
+data_full_cdr3<-data_full_cdr3[which(data_full_cdr3$CDR3_length!=3),] #2,087,010
+
+##Obtain the ID for the clone call
+data_full_cdr3$seqID<-seq(1,nrow(data_full_cdr3))
+data_full_cdr3$SEQUENCE_ID<-paste(data_full_cdr3$sample,data_full_cdr3$seqID,sep="_")
+
+##Variable to build clones
+data_full_cdr3$V_J_lenghCDR3 = paste(data_full_cdr3$bestVGene,data_full_cdr3$bestJGene,data_full_cdr3$CDR3_length,sep="_")
+
+###save the data to call the clones by all samples using the nucleotides.py
+data_clonesInference<-data_full_cdr3[,c("SEQUENCE_ID","sample","nSeqCDR3","CDR3_length","bestVGene","bestJGene","V_J_lenghCDR3")]
+write.table(data_clonesInference,file="Data/PAAD/data_for_cloneInfered_PAAD.txt",row.names = F,sep="\t")
+
+### After passing the nucleotides.py
+##Read the clones and merge with the data
+nucleotides<-read.csv("Data/PAAD/ClonesInfered_PAAD.csv")
+data_merge<-merge(data_full_cdr3,nucleotides[,c("SEQUENCE_ID","CloneId")],by=c("SEQUENCE_ID"))
+
+###Add a column with the reads per chain for Ig and TRA
+data_merge$chainType<-substr(data_merge$bestVGene,1,4)
+##Clones per chain
+data_merge$V_J_lenghCDR3_CloneId = paste(data_merge$V_J_lenghCDR3,data_merge$CloneId,sep="_")
+clones_count<- unique(data_merge[,c("sample","V_J_lenghCDR3_CloneId","chainType")])
+clones<-data.matrix(table(clones_count$sample,clones_count$chainType))
+colnames(clones)<-c("clones_IGH","clones_IGK","clones_IGL","clones_TRA","clones_TRB","clones_TRD","clones_TRG")
+
+##Diversity measures
+sample<-rownames(reads_clones_annot)
+entropy_IGH<-NULL
+entropy_IGK<-NULL
+entropy_IGL<-NULL
+entropy_TRA<-NULL
+entropy_TRB<-NULL
+entropy_TRD<-NULL
+entropy_TRG<-NULL
+for (i in 1:length(sample)){
+  print(i)
+  data_sample_unique<-data_merge[which(data_merge$sample==sample[i]),]
+  clones_sample<-data_sample_unique[,"V_J_lenghCDR3_CloneId"]
+  clones_sample_IGH<-data_sample_unique[which(data_sample_unique$chainType=="IGHV"),"V_J_lenghCDR3_CloneId"]
+  clones_sample_IGK<-data_sample_unique[which(data_sample_unique$chainType=="IGKV"),"V_J_lenghCDR3_CloneId"]
+  clones_sample_IGL<-data_sample_unique[which(data_sample_unique$chainType=="IGLV"),"V_J_lenghCDR3_CloneId"]
+  clones_sample_TRA<-data_sample_unique[which(data_sample_unique$chainType=="TRAV"),"V_J_lenghCDR3_CloneId"]
+  clones_sample_TRB<-data_sample_unique[which(data_sample_unique$chainType=="TRBV"),"V_J_lenghCDR3_CloneId"]
+  clones_sample_TRD<-data_sample_unique[which(data_sample_unique$chainType=="TRDV"),"V_J_lenghCDR3_CloneId"]
+  clones_sample_TRG<-data_sample_unique[which(data_sample_unique$chainType=="TRGV"),"V_J_lenghCDR3_CloneId"]
+  
+  #To write file to run with Recon
+  write.delim(data.frame(table(table(clones_sample_IGH))),file=paste("clones_sample_IGH_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  write.delim(data.frame(table(table(clones_sample_IGK))),file=paste("clones_sample_IGK_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  write.delim(data.frame(table(table(clones_sample_IGL))),file=paste("clones_sample_IGL_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  write.delim(data.frame(table(table(clones_sample_TRA))),file=paste("clones_sample_TRA_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  write.delim(data.frame(table(table(clones_sample_TRB))),file=paste("clones_sample_TRB_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  write.delim(data.frame(table(table(clones_sample_TRD))),file=paste("clones_sample_TRD_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  write.delim(data.frame(table(table(clones_sample_TRG))),file=paste("clones_sample_TRG_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  
+  fi_IGH<-as.numeric(table(clones_sample_IGH))/length(clones_sample_IGH)
+  fi_IGK<-as.numeric(table(clones_sample_IGK))/length(clones_sample_IGK)
+  fi_IGL<-as.numeric(table(clones_sample_IGL))/length(clones_sample_IGL)
+  fi_TRA<-as.numeric(table(clones_sample_TRA))/length(clones_sample_TRA)
+  fi_TRB<-as.numeric(table(clones_sample_TRB))/length(clones_sample_TRB)
+  fi_TRD<-as.numeric(table(clones_sample_TRD))/length(clones_sample_TRD)
+  fi_TRG<-as.numeric(table(clones_sample_TRG))/length(clones_sample_TRG)
+  
+  hi_IGH<-fi_IGH*log2(fi_IGH)
+  hi_IGK<-fi_IGK*log2(fi_IGK)
+  hi_IGL<-fi_IGL*log2(fi_IGL)
+  hi_TRA<-fi_TRA*log2(fi_TRA)
+  hi_TRB<-fi_TRB*log2(fi_TRB)
+  hi_TRD<-fi_TRD*log2(fi_TRD)
+  hi_TRG<-fi_TRG*log2(fi_TRG)
+  
+  entropy_IGH[i]=-sum(hi_IGH)
+  entropy_IGK[i]=-sum(hi_IGK)
+  entropy_IGL[i]=-sum(hi_IGL)
+  entropy_TRA[i]=-sum(hi_TRA)
+  entropy_TRB[i]=-sum(hi_TRB)
+  entropy_TRD[i]=-sum(hi_TRD)
+  entropy_TRG[i]=-sum(hi_TRG)
+  
+}
+
+diversity<-cbind(clones,entropy_IGH,entropy_IGK,entropy_IGL,entropy_TRA,entropy_TRB,entropy_TRD,entropy_TRG)
+save(data_merge,diversity,file="Data/PAAD/PAAD_RepertoireResults_clones.Rdata")
+
+
+####Read repertoire data from Akshay considering all reads
+## It is important to remember that when retricting reads to the ones with cdr3 informartion, the ratios where pretty weird since there were very few reads in the most uncommon chains
+## Therefore for the chain expression, we are going to stay with all reads detected by the MISCR tool
 PAAD_repertoire<-readRDS("Data/TCGA_Immune_Rep/PAAD_RepertoireResults.rds")
-file_ids = rownames(PAAD_repertoire)
+id<-match(rownames(diversity),rownames(PAAD_repertoire))
+PAAD_repertoire_diversity<-cbind(PAAD_repertoire[id,],diversity)
+
+file_ids = rownames(PAAD_repertoire_diversity)
 annotation = TCGAtranslateID(file_ids)
 annotation$patient_barcode = substr(annotation$submitter_id,1,12)
 write.csv(annotation,"Data/PAAD/annotation_PAAD.csv") ###To mark the non pancreas based on http://clincancerres.aacrjournals.org/content/clincanres/early/2018/05/08/1078-0432.CCR-18-0290.full.pdf
 annotation<-read.csv("Data/PAAD/annotation_PAAD.csv")
-id<-match(rownames(PAAD_repertoire),annotation$file_id)
-rownames(PAAD_repertoire)<-annotation$submitter_id[id]
-PAAD_repertoire$Tumor_type<-annotation$tumor_type[id]
+id<-match(rownames(PAAD_repertoire_diversity),annotation$file_id)
+PAAD_repertoire_diversity$TCGA_sample<-annotation$submitter_id[id]
+PAAD_repertoire_diversity$Tumor_type<-annotation$tumor_type[id]
 
 ####CLinical data
 library(TCGAbiolinks)
@@ -97,7 +205,7 @@ biospecimen.slide<-biospecimen.slide[,-c(2,4,10,13,15)]
 write.csv(biospecimen.slide,"Data/PAAD/Clinical/biospecimen_slide_PAAD.csv")
 
 ###Read xCELL data
-xCell.data<-read.csv("Data/xCell_TCGA_RSEM.csv")
+xCell.data<-read.table("Data/xCELL/xCell_PAAD_fpkm_xCell_0725041119.txt",header = T,sep="\t")
 rownames(xCell.data)<-xCell.data$X
 xCell.data<-xCell.data[,-1]
 xx<-str_replace(colnames(xCell.data),"\\.","-")
@@ -105,8 +213,8 @@ xx<-str_replace(xx,"\\.","-")
 xx<-str_replace(xx,"\\.","-")
 colnames(xCell.data)<-xx
 write.csv(xCell.data,"Data/xCell_TCGA.csv")
-id.xcell<-match(substring(rownames(PAAD_repertoire),1,15),colnames(xCell.data))
-xCell.data.PAAD<-xCell.data[,na.omit(id.xcell)]
+id.xcell<-match(PAAD_repertoire_diversity$TCGA_sample,colnames(xCell.data))
+xCell.data.PAAD<-xCell.data[,na.omit(id.xcell)] ##180 
 
 save(PAAD_repertoire,xCell.data.PAAD,clinical.drug,clinical.patient,clinical.radiation,clinical.new_tumor_event,clinical.folow_up,biospecimen.slide,annotation,
      file="Data/PAAD/PAAD_repertoire_xCell_clinical.Rdata")
