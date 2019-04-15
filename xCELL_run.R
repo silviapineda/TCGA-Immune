a3032bebf7f57ce77ba3fea9af22ac611ca135ef
silviapineda <silviapinedasanjuan@gmail.com>
2019-04-15 17:57:36 +0200

    Preparation for the files used for the xcell tool

--- xCELL_run.R
+++ xCELL_run.R
@@ -1,7 +1,21 @@
 
-PAAD_fpkm<-read.table("~/TCGA-Immune/Data/xCELL/TCGA-PAAD.htseq_fpkm.tsv",header = T)
-genecode<-read.table("~/TCGA-Immune/Data/xCELL/gencode.v22.annotation.gene.probeMap",header = T)
+###Download the RSEM FPKM values from the xenabrowser
+#https://xenabrowser.net/datapages/?dataset=tcga_RSEM_gene_fpkm&host=https%3A%2F%2Ftoil.xenahubs.net&removeHub=https%3A%2F%2Fxena.treehouse.gi.ucsc.edu%3A443
+xcell<-read.csv("Data/xCELL/tcga_RSEM_gene_fpkm.txt",sep="\t")
+xx<-str_replace(colnames(xcell),"\\.","-")
+xx<-str_replace(xx,"\\.","-")
+xx<-str_replace(xx,"\\.","-")
 
-id.gene<-match(PAAD_fpkm$Ensembl_ID,genecode$id)
-PAAD_fpkm$Gene<-genecode$gene[id.gene]
-write.csv(PAAD_fpkm,"~/TCGA-Immune/Data/xCELL/PAAD_fpkm.csv")
+load("Data/PAAD/PAAD_RepertoireResults_clones.Rdata")
+RSEM_PAAD<-xcell[,match(substr(as.character(PAAD_repertoire_diversity$TCGA_sample),1,15),xx)]
+rownames(RSEM_PAAD)<-xcell$sample
+
+##Annotation file
+genecode<-read.table("~/TCGA-Immune/Data/xCELL/gencode.v23.annotation.gene.probemap.txt",header = T)
+
+id.gene<-match(rownames(RSEM_PAAD),genecode$id)
+RSEM_PAAD$Gene<-genecode$gene[id.gene]
+write.csv(RSEM_PAAD,"~/TCGA-Immune/Data/xCELL/PAAD_rsem.csv")
+
+###Run xcell tool to obtain the de-convolution
+#http://xcell.ucsf.edu/
