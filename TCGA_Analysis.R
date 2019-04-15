b02aeff025b4d7b34fe34073f9e737cb8bfbec00
silviapineda <silviapinedasanjuan@gmail.com>
2019-04-15 17:58:01 +0200

    Preliminar analysis

--- TCGA_Analysis.R
+++ TCGA_Analysis.R
@@ -17,23 +17,31 @@ print(x)
 
 setwd("~/TCGA-Immune/")
 
-load("Data/PAAD/PAAD_repertoire_xCell_clinical.Rdata")
+load("Data/PAAD/PAAD_FullData.Rdata")
+PAAD_repertoire_diversity$Tumor_type_2categ<-ifelse(PAAD_repertoire_diversity$Tumor_type=="Tumor_pancreas","Tumor_pancres",
+                                             ifelse(PAAD_repertoire_diversity$Tumor_type=="Solid_tissue_normal","Adjacent_normal_pancreas",
+                                             ifelse(PAAD_repertoire_diversity$Tumor_type=="Adjacent_normal_pancreas","Adjacent_normal_pancreas","Other")))
 
-boxplot(PAAD_repertoire$Alpha_Beta_ratio_expression~PAAD_repertoire$Tumor_type,las=2)
+####Descriptive analysis to see if there are differences by tumor and adjacent_normal
+p.value<-NULL
+for(i in 1:(ncol(PAAD_repertoire_diversity)-3)){
+  p.value[i]<-coef(summary(glm(PAAD_repertoire_diversity[,i]~PAAD_repertoire_diversity$Tumor_type_2categ)))[3,4]
+}
+boxplot(PAAD_repertoire_diversity$entropy_recon_IGK~PAAD_repertoire_diversity$Tumor_type_2categ)
 
 ##Only tumor pancreas
-PAAD_repertoire_tumor<-PAAD_repertoire[which(PAAD_repertoire$Tumor_type=="Tumor_pancreas"),]
-PAAD_repertoire_tumor$bcr_patient_barcode<-substr(rownames(PAAD_repertoire_tumor),1,12)
+PAAD_repertoire_tumor<-PAAD_repertoire_diversity[which(PAAD_repertoire_diversity$Tumor_type=="Tumor_pancreas"),]
+PAAD_repertoire_tumor$bcr_patient_barcode<-substr(PAAD_repertoire_tumor$TCGA_sample,1,12)
 
 ##Merge with xCell
 xCell.data.PAAD<-data.frame(t(xCell.data.PAAD))
 xCell.data.PAAD$bcr_patient_barcode<-substr(rownames(xCell.data.PAAD),1,12)
 xcell.repertoire.data<-merge(PAAD_repertoire_tumor,xCell.data.PAAD,by="bcr_patient_barcode")
 
-xcell.data<-xcell.repertoire.data[,24:length(colnames(xcell.repertoire.data))]
+xcell.data<-xcell.repertoire.data[,54:length(colnames(xcell.repertoire.data))]
 p.value=NULL
 for (i in 1:length(colnames(xcell.data))){
-  p.value[i]<-coef(summary(glm(xcell.repertoire.data$IG_expression~xcell.data[,i])))[2,4]
+  p.value[i]<-coef(summary(glm(xcell.repertoire.data$clones_recon_TRA~xcell.data[,i])))[2,4]
 }
 xcell.data[,which(p.adjust(p.value)<0.05)]
 
