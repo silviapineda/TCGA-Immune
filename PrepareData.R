1096794c95dae2f6170864fd5f26cf128be9f639
silviapineda <silviapinedasanjuan@gmail.com>
2019-05-21 16:57:46 +0200

    The call for the clones need to be separated by IG and TCR

--- PrepareData.R
+++ PrepareData.R
@@ -44,19 +44,33 @@ data_full_cdr3$SEQUENCE_ID<-paste(data_full_cdr3$sample,data_full_cdr3$seqID,sep
 ##Variable to build clones
 data_full_cdr3$V_J_lenghCDR3 = paste(data_full_cdr3$bestVGene,data_full_cdr3$bestJGene,data_full_cdr3$CDR3_length,sep="_")
 
+###Chain
+data_full_cdr3$chainType<-substr(data_full_cdr3$bestVGene,1,4)
+data_full_cdr3_Ig<-data_full_cdr3[which(data_full_cdr3$chainType=="IGHV" |
+                                        data_full_cdr3$chainType=="IGKV" |
+                                        data_full_cdr3$chainType=="IGLV"),]
+data_full_cdr3_TCR<-data_full_cdr3[which(data_full_cdr3$chainType=="TRAV" | 
+                                         data_full_cdr3$chainType=="TRBV" |
+                                         data_full_cdr3$chainType=="TRDV" | 
+                                         data_full_cdr3$chainType=="TRGV"),]
+
 ###save the data to call the clones by all samples using the nucleotides.py
-data_clonesInference<-data_full_cdr3[,c("SEQUENCE_ID","sample","nSeqCDR3","CDR3_length","bestVGene","bestJGene","V_J_lenghCDR3")]
-write.table(data_clonesInference,file="Data/PAAD/data_for_cloneInfered_PAAD.txt",row.names = F,sep="\t")
+data_clonesInference_Ig<-data_full_cdr3_Ig[,c("SEQUENCE_ID","sample","nSeqCDR3","CDR3_length","bestVGene","bestJGene","V_J_lenghCDR3")]
+data_clonesInference_TCR<-data_full_cdr3_TCR[,c("SEQUENCE_ID","sample","nSeqCDR3","CDR3_length","bestVGene","bestJGene","V_J_lenghCDR3")]
+write.table(data_clonesInference_Ig,file="Data/PAAD/data_for_cloneInfered_Ig_PAAD.txt",row.names = F,sep="\t")
+write.table(data_clonesInference_TCR,file="Data/PAAD/data_for_cloneInfered_TCR_PAAD.txt",row.names = F,sep="\t")
 
 ### After passing the nucleotides.py
 ##Read the clones and merge with the data
-nucleotides<-read.csv("Data/PAAD/ClonesInfered_PAAD.csv")
+nucleotides_Ig<-read.csv("Data/PAAD/ClonesInfered_PAAD_Ig.csv")
+nucleotides_TCR<-read.csv("Data/PAAD/ClonesInfered_PAAD_TCR.csv")
+nucleotides<-rbind(nucleotides_Ig,nucleotides_TCR)
 data_merge<-merge(data_full_cdr3,nucleotides[,c("SEQUENCE_ID","CloneId")],by=c("SEQUENCE_ID"))
 
 ###Add a column with the reads per chain for Ig and TRA
-data_merge$chainType<-substr(data_merge$bestVGene,1,4)
 ##Clones per chain
 data_merge$V_J_lenghCDR3_CloneId = paste(data_merge$V_J_lenghCDR3,data_merge$CloneId,sep="_")
+
 clones_count<- unique(data_merge[,c("sample","V_J_lenghCDR3_CloneId","chainType")])
 clones<-data.matrix(table(clones_count$sample,clones_count$chainType))
 colnames(clones)<-c("clones_IGH","clones_IGK","clones_IGL","clones_TRA","clones_TRB","clones_TRD","clones_TRG")
@@ -83,13 +97,13 @@ for (i in 1:length(sample)){
   clones_sample_TRG<-data_sample_unique[which(data_sample_unique$chainType=="TRGV"),"V_J_lenghCDR3_CloneId"]
   
   #To write file to run with Recon
-  write.delim(data.frame(table(table(clones_sample_IGH))),file=paste("clones_sample_IGH_",sample[i],".txt",sep=""),sep="\t",col.names=F)
-  write.delim(data.frame(table(table(clones_sample_IGK))),file=paste("clones_sample_IGK_",sample[i],".txt",sep=""),sep="\t",col.names=F)
-  write.delim(data.frame(table(table(clones_sample_IGL))),file=paste("clones_sample_IGL_",sample[i],".txt",sep=""),sep="\t",col.names=F)
-  write.delim(data.frame(table(table(clones_sample_TRA))),file=paste("clones_sample_TRA_",sample[i],".txt",sep=""),sep="\t",col.names=F)
-  write.delim(data.frame(table(table(clones_sample_TRB))),file=paste("clones_sample_TRB_",sample[i],".txt",sep=""),sep="\t",col.names=F)
-  write.delim(data.frame(table(table(clones_sample_TRD))),file=paste("clones_sample_TRD_",sample[i],".txt",sep=""),sep="\t",col.names=F)
-  write.delim(data.frame(table(table(clones_sample_TRG))),file=paste("clones_sample_TRG_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  #write.delim(data.frame(table(table(clones_sample_IGH))),file=paste("clones_sample_IGH_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  #write.delim(data.frame(table(table(clones_sample_IGK))),file=paste("clones_sample_IGK_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  #write.delim(data.frame(table(table(clones_sample_IGL))),file=paste("clones_sample_IGL_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  #write.delim(data.frame(table(table(clones_sample_TRA))),file=paste("clones_sample_TRA_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  #write.delim(data.frame(table(table(clones_sample_TRB))),file=paste("clones_sample_TRB_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  #write.delim(data.frame(table(table(clones_sample_TRD))),file=paste("clones_sample_TRD_",sample[i],".txt",sep=""),sep="\t",col.names=F)
+  #write.delim(data.frame(table(table(clones_sample_TRG))),file=paste("clones_sample_TRG_",sample[i],".txt",sep=""),sep="\t",col.names=F)
   
   fi_IGH<-as.numeric(table(clones_sample_IGH))/length(clones_sample_IGH)
   fi_IGK<-as.numeric(table(clones_sample_IGK))/length(clones_sample_IGK)
@@ -121,36 +135,36 @@ diversity<-cbind(clones,entropy_IGH,entropy_IGK,entropy_IGL,entropy_TRA,entropy_
 
 ####After runing recon
 recon<-read.table("Data/PAAD/RECON/test_D_number_table.txt",header=T)
-chain<-substr(recon$sample_name,61,64)
-sample<-substr(recon$sample_name,66,101)
+chain<-substr(recon$sample_name,61,63)
+sample<-substr(recon$sample_name,65,100)
 ##0.0D is species richness (Number of clones)
 ##Entropy is ln(1.0.D)
 diversity<-as.data.frame(diversity)
 chain_list<-unique(chain)
 for(i in chain_list){
   recon_chain<-recon[which(chain==i),]
-  sample_chain<-substr(recon_chain$sample_name,66,101)
+  sample_chain<-substr(recon_chain$sample_name,65,100)
   id<-match(rownames(diversity),sample_chain)
   clone_chain<-ifelse(is.na(id)==F,recon_chain[id,"est_0.0D"],0)
   assign(paste0("clones_recon_",i),clone_chain)
   entroy_chain<-ifelse(is.na(id)==F,ln(recon_chain[id,"est_1.0D"]),0)
   assign(paste0("entropy_recon_",i),entroy_chain)
 }
-diversity$clones_recon_IGH<-clones_recon_IGHV
-diversity$clones_recon_IGK<-clones_recon_IGKV
-diversity$clones_recon_IGL<-clones_recon_IGLV
-diversity$clones_recon_TRA<-clones_recon_TRAV
-diversity$clones_recon_TRB<-clones_recon_TRBV
-diversity$clones_recon_TRD<-clones_recon_TRDV
-diversity$clones_recon_TRG<-clones_recon_TRGV
+diversity$clones_recon_IGH<-clones_recon_IGH
+diversity$clones_recon_IGK<-clones_recon_IGK
+diversity$clones_recon_IGL<-clones_recon_IGL
+diversity$clones_recon_TRA<-clones_recon_TRA
+diversity$clones_recon_TRB<-clones_recon_TRB
+diversity$clones_recon_TRD<-clones_recon_TRD
+diversity$clones_recon_TRG<-clones_recon_TRG
 
-diversity$entropy_recon_IGH<-entropy_recon_IGHV
-diversity$entropy_recon_IGK<-entropy_recon_IGKV
-diversity$entropy_recon_IGL<-entropy_recon_IGLV
-diversity$entropy_recon_TRA<-entropy_recon_TRAV
-diversity$entropy_recon_TRB<-entropy_recon_TRBV
-diversity$entropy_recon_TRD<-entropy_recon_TRDV
-diversity$entropy_recon_TRG<-entropy_recon_TRGV
+diversity$entropy_recon_IGH<-entropy_recon_IGH
+diversity$entropy_recon_IGK<-entropy_recon_IGK
+diversity$entropy_recon_IGL<-entropy_recon_IGL
+diversity$entropy_recon_TRA<-entropy_recon_TRA
+diversity$entropy_recon_TRB<-entropy_recon_TRB
+diversity$entropy_recon_TRD<-entropy_recon_TRD
+diversity$entropy_recon_TRG<-entropy_recon_TRG
 #Simpson Index (1/2D) and BPI (1/∞D)
 save(data_merge,diversity,file="Data/PAAD/PAAD_RepertoireResults_diversity.Rdata")
 
