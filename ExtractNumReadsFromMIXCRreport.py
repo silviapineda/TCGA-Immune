e8fcc39008caeedb7ab857b4cd6074499c061ea2
silviapineda <silviapinedasanjuan@gmail.com>
2019-06-17 16:52:04 +0200

    To extract the number of reads from the report MIXCR

---
+++ ExtractNumReadsFromMIXCRreport.py
@@ -0,0 +1,19 @@
+import glob
+
+###Read the file in gtf 
+filenames = glob.glob("/Users/Pinedasans/TCGA-Immune/Data/PAAD/MIXCR_PAAD/reports/*alignments_report.txt")
+print("start")
+fout = open("/Users/Pinedasans/TCGA-Immune/Data/PAAD/MIXCR_PAAD/total_reads.txt",'w')
+for f in filenames:
+    print(f)
+    sample = f[59:95]
+    print(sample)
+    fh = open(f)
+    for line in fh:
+        line=line.rstrip()
+        if line.startswith("Total sequencing reads"):
+            words = line.split()
+            print(words[3])
+            fout.write(sample + ';' + words[3] + '\n')
+            continue
+print("Done")
