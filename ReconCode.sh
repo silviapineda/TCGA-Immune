27d762c2843cce8ce567c5d15c93dd71fb40212c
silviapineda <silviapinedasanjuan@gmail.com>
2019-08-08 17:03:33 +0200

    First commit

---
+++ ReconCode.sh
@@ -0,0 +1,25 @@
+#!/
+
+bin/bash
+
+FILES=/Users/Pinedasans/TCGA-Immune/Data/Pancreas_Validation/Recon/*.txt
+for f in $FILES
+do  
+   echo $f
+   python2.7 recon_v2.2.py -R -c -t 30 -o $f.fitfile.txt $f
+   #python2.7 recon_v2.2.py -x --x_max 30 -o $f.plotfile.txt -b error_bar_parameters.txt $f.fitfile.txt
+done
+
+
+FILES=/Users/Pinedasans/TCGA-Immune/Data/Pancreas_Validation/Recon/*fitfile.txt
+
+variantParameters=
+for f in $FILES
+do
+    echo $f
+    variantParameters="$variantParameters $f"
+done
+
+echo $variantParameters
+python2.7 recon_v2.2.py -D -Q 0 1 2 inf -b error_bar_parameters.txt -o /Users/Pinedasans/TCGA-Immune/Data/Pancreas_Validation/Recon/test_D_number_table.txt $variantParameters
+
