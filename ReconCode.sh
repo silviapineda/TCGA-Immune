47e5c4560841f955cb2667673d1912628e00e95e
silviapineda <silviapinedasanjuan@gmail.com>
2019-08-21 15:43:00 +0200

    directpory change

--- ReconCode.sh
+++ ReconCode.sh
@@ -2,16 +2,15 @@
 
 bin/bash
 
-FILES=/Users/Pinedasans/TCGA-Immune/Data/GTEx/Pancreas/Recon/*.txt
+FILES=/Users/Pinedasans/TCGA-Immune/Data/PAAD_VAL/RECON/*.txt
 for f in $FILES
 do  
    echo $f
    python2.7 ~/programs/Recon-master2/recon_v2.2.py -R -c -t 30 -o $f.fitfile.txt $f
-   #python2.7 recon_v2.2.py -x --x_max 30 -o $f.plotfile.txt -b error_bar_parameters.txt $f.fitfile.txt
-done
+ done
 
 
-FILES=/Users/Pinedasans/TCGA-Immune/Data/GTEx/Pancreas/Recon/*fitfile.txt
+FILES=/Users/Pinedasans/TCGA-Immune/Data/PAAD_VAL/RECON/*fitfile.txt
 
 variantParameters=
 for f in $FILES
@@ -21,5 +20,5 @@ do
 done
 
 echo $variantParameters
-python2.7 ~/programs/Recon-master2/recon_v2.2.py -D -Q 0 1 2 inf -b ~/programs/Recon-master2/error_bar_parameters.txt -o /Users/Pinedasans/TCGA-Immune/Data/GTEx/Pancreas/Recon/test_D_number_table.txt $variantParameters
+python2.7 ~/programs/Recon-master2/recon_v2.2.py -D -Q 0 1 2 inf -b ~/programs/Recon-master2/error_bar_parameters.txt -o /Users/Pinedasans/TCGA-Immune/Data/PAAD_VAL/RECON/test_D_number_table.txt $variantParameters
 
