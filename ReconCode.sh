aa70dc3732e411c4f69cbf6a032916c332c0c210
silviapineda <silviapinedasanjuan@gmail.com>
2019-08-27 15:49:30 +0200

    dir

--- ReconCode.sh
+++ ReconCode.sh
@@ -2,7 +2,7 @@
 
 bin/bash
 
-FILES=/Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/*.txt
+FILES=/Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/Down/*.txt
 for f in $FILES
 do  
    echo $f
@@ -10,7 +10,7 @@ do
  done
 
 
-FILES=/Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/*fitfile.txt
+FILES=/Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/Down/*fitfile.txt
 
 variantParameters=
 for f in $FILES
@@ -20,5 +20,5 @@ do
 done
 
 echo $variantParameters
-python2.7 ~/programs/Recon-master2/recon_v2.2.py -D -Q 0 1 2 inf -b ~/programs/Recon-master2/error_bar_parameters.txt -o /Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/test_D_number_table.txt $variantParameters
+python2.7 ~/programs/Recon-master2/recon_v2.2.py -D -Q 0 1 2 inf -b ~/programs/Recon-master2/error_bar_parameters.txt -o /Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/Down/test_D_number_table.txt $variantParameters
 
