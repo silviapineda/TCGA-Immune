12ef5b3a16c9d62bcd017ab20414f035c38e4e83
silviapineda <silviapinedasanjuan@gmail.com>
2019-09-02 16:51:00 +0200

    dir

--- ReconCode.sh
+++ ReconCode.sh
@@ -1,16 +1,14 @@
-#!/
+#!/bin/bash
 
-bin/bash
-
-FILES=/Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/Down/*.txt
-for f in $FILES
-do  
-   echo $f
-   python2.7 ~/programs/Recon-master2/recon_v2.2.py -R -c -t 30 -o $f.fitfile.txt $f
- done
+#FILES=/Users/Pinedasans/TCGA-Immune/Data/GTEx/Blood/RECON/*.txt
+#for f in $FILES
+#do  
+#   echo $f
+#   python2.7 ~/programs/Recon-master2/recon_v2.2.py -R -c -t 30 -o $f.fitfile.txt $f
+# done
 
 
-FILES=/Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/Down/*fitfile.txt
+FILES=/Users/Pinedasans/TCGA-Immune/Data/GTEx/Blood/RECON/*TR*fitfile.txt
 
 variantParameters=
 for f in $FILES
@@ -20,5 +18,5 @@ do
 done
 
 echo $variantParameters
-python2.7 ~/programs/Recon-master2/recon_v2.2.py -D -Q 0 1 2 inf -b ~/programs/Recon-master2/error_bar_parameters.txt -o /Users/Pinedasans/TCGA-Immune/Data/PAAD_GTEx/RECON/Down/test_D_number_table.txt $variantParameters
+python2.7 ~/programs/Recon-master2/recon_v2.2.py -D -Q 0 1 2 inf -b ~/programs/Recon-master2/error_bar_parameters.txt -o /Users/Pinedasans/TCGA-Immune/Data/GTEx/Blood/RECON/test_D_number_table_TR.txt $variantParameters
 
